e03b44d627cf65535409a6f62a5c3e86e98d506e
Nizar ABAK-KALI <nak@nrobotix.com>
2016-09-30 16:38:55 +0200

    first push de l'application

---
+++ app/src/androidTest/java/fr/p8/m2ise/androidfacecamrecog/ApplicationTest.java
@@ -0,0 +1,13 @@
+package fr.p8.m2ise.androidfacecamrecog;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
