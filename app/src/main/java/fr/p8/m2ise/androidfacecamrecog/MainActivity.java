9ec7c1b70b7bf37aedf19b63e0f24c3a8f2e23a7
Zhaleh <yekta.jaleh@gmail.com>
2016-10-21 10:34:43 +0200

    creation add post add updateuser

--- app/src/main/java/fr/p8/m2ise/androidfacecamrecog/MainActivity.java
+++ app/src/main/java/fr/p8/m2ise/androidfacecamrecog/MainActivity.java
@@ -1,8 +1,8 @@
 package fr.p8.m2ise.androidfacecamrecog;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
@@ -32,6 +32,4 @@ public class MainActivity extends AppCompatActivity{
         });
 
     }
-
-
 }
