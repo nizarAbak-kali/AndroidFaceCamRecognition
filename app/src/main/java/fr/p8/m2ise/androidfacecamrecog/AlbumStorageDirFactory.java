2df0ecd7a2975c1b046a5282f0f9bd7c1f9c2a39
Zhaleh <yekta.jaleh@gmail.com>
2016-11-07 00:56:08 +0100

    Crée AlbumStorageDirFactory (Pour test) Ajouter fonction onCreate pour ImageView Ajouter onActivityResult Ajouter Decode JPEG pour Bitmap cree une fonction pour add pic to garllery(pour test sa marche bien ou pas ) cree une class pour fonction AlbumeStorage

---
+++ app/src/main/java/fr/p8/m2ise/androidfacecamrecog/AlbumStorageDirFactory.java
@@ -0,0 +1,9 @@
+package fr.p8.m2ise.androidfacecamrecog;
+import java.io.File;
+/**
+ * Created by zhaleh on 16/11/06.
+ */
+
+abstract class AlbumStorageDirFactory {
+    public abstract File getAlbumStorageDir(String albumName);
+}
